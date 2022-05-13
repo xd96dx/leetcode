130dc58b79e00c632e581592a12ea5aaf5e34699
xd <xd@123.com>
2022-05-13 14:45:43 +0800

    add notes

--- leetcode1115.cpp
+++ leetcode1115.cpp
@@ -1,12 +1,33 @@
-//
-// Created by xd on 5/13/22.
-//
+/*
+1115. 交替打印 FooBar
+给你一个类：
+
+class FooBar {
+  public void foo() {
+    for (int i = 0; i < n; i++) {
+      print("foo");
+    }
+  }
+
+  public void bar() {
+    for (int i = 0; i < n; i++) {
+      print("bar");
+    }
+  }
+}
+两个不同的线程将会共用一个 FooBar 实例：
+
+线程 A 将会调用 foo() 方法，而
+线程 B 将会调用 bar() 方法
+请设计修改程序，以确保 "foobar" 被输出 n 次。
+*/
 
 #include <iostream>
 #include <mutex>
 #include <thread>
 #include <condition_variable>
 
+// 这个题用atomic, 做也可以, 先用了普通的condition 做了一版, 没啥难度吧感觉...
 class test {
 public:
     std::condition_variable cv;
